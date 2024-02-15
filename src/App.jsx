577878b6be8d05508eae5b54a52ff0f345196166
MadaraUshiwa <abdelbenlaziz@gmail.com>
2024-02-15 14:22:15 +0100

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Connexion from './components/Connexion/Connexion';
-import Home from './components/Home/Home';
+
 
 
 function App() {
     return (
         <div className='App'>
-            <Home /> 
         </div>
     );
 }
